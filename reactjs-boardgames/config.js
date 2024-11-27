02b137d3c3a342140ce15d75d61bafd79cb795d8
david-gimelle <david.gimelle2@hmcts.net>
2024-11-27 07:08:53 +0000

    add PW test

--- reactjs-boardgames/config.js
+++ reactjs-boardgames/config.js
@@ -1,7 +1,8 @@
 const config = {
-    nodeApiUrl: 'http://127.0.0.1:5001/boardgames',
-    pythonApiUrl: 'http://localhost:3000/boardgames',
+    nodeApiUrl: 'http://127.0.0.1:3000/boardgames',
+    pythonApiUrl: 'http://localhost:5001/boardgames',
     javaApiUrl: 'http://localhost:8082/boardgames',
+    reactJSAppPort: 5175,
   };
   
   export default config;
