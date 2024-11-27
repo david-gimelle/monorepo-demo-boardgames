751f15ad2c79b9a91d2cea388ab3107378de1a9d
david-gimelle <david.gimelle2@hmcts.net>
2024-11-27 07:17:09 +0000

    Fix e22 tests

--- nodejs-boardgames-api/config.ts
+++ nodejs-boardgames-api/config.ts
@@ -1,6 +1,6 @@
 const config = {
     port: parseInt(process.env.PORT || '3000', 10),
-    corsAllowedOrigin: process.env.CORS_ALLOWED_ORIGIN || 'http://localhost:5173',
+    corsAllowedOrigin: process.env.CORS_ALLOWED_ORIGIN || 'http://localhost:5175',
   };
   
   export default config;
