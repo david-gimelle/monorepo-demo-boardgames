f5d49dfd072c6cdc9faab962b8ee092b1d4448bd
david-gimelle <david.gimelle@gmail.com>
2025-01-03 12:42:49 +0000

    init terraform files

---
+++ envs/dev/main.tf
@@ -0,0 +1,44 @@
+provider "google" {
+  project = var.project_id
+  region  = var.region
+}
+
+resource "google_container_cluster" "primary" {
+  name     = var.cluster_name
+  location = var.region
+
+  initial_node_count = 1
+
+  node_config {
+    machine_type = "e2-medium"
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform",
+    ]
+  }
+}
+
+resource "google_container_node_pool" "primary_nodes" {
+  cluster    = google_container_cluster.primary.name
+  location   = google_container_cluster.primary.location
+  node_count = 1
+
+  node_config {
+    preemptible  = true
+    machine_type = "e2-medium"
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform",
+    ]
+  }
+}
+
+output "kubernetes_cluster_name" {
+  value = google_container_cluster.primary.name
+}
+
+output "kubernetes_cluster_endpoint" {
+  value = google_container_cluster.primary.endpoint
+}
+
+output "kubernetes_cluster_ca_certificate" {
+  value = google_container_cluster.primary.master_auth.0.cluster_ca_certificate
+}
---
+++ envs/dev/variables.tf
@@ -0,0 +1,16 @@
+variable "project_id" {
+  description = "The ID of the GCP project"
+  type        = string
+}
+
+variable "region" {
+  description = "The region to deploy the GKE cluster"
+  type        = string
+  default     = "us-central1"
+}
+
+variable "cluster_name" {
+  description = "The name of the GKE cluster"
+  type        = string
+  default     = "my-gke-cluster"
+}
