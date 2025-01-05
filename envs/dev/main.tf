798cfa184d181603c7ad983b53075ab62118f13d
david-gimelle <david.gimelle@gmail.com>
2025-01-05 11:32:17 +0000

    Revert "autopilot cluster"
    This reverts commit 12636a66eb95866b40a498ff4b34b097da0089f2.

--- envs/dev/main.tf
+++ envs/dev/main.tf
@@ -14,10 +14,8 @@ resource "google_container_cluster" "primary" {
   name     = var.cluster_name
   location = var.region
 
-  autopilot {
-    enabled = true
-  }
-
+  initial_node_count = 1
+  remove_default_node_pool = true
   min_master_version = "1.30.6-gke.1125000"
   release_channel {
     channel = "REGULAR"
@@ -42,6 +40,55 @@ resource "google_container_cluster" "primary" {
     dns_cache_config {
       enabled = true
     }
+  }
+
+  node_config {
+    machine_type = "e2-micro"
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform",
+    ]
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+    shielded_instance_config {
+      enable_secure_boot = true
+    }
+  }
+
+  timeouts {
+    create = "30m"
+    update = "30m"
+    delete = "30m"
+  }
+}
+
+resource "google_container_node_pool" "primary_nodes" {
+  cluster    = google_container_cluster.primary.name
+  location   = google_container_cluster.primary.location
+  node_count = 1
+
+  node_config {
+    preemptible  = true
+    machine_type = "e2-micro"
+    oauth_scopes = [
+      "https://www.googleapis.com/auth/cloud-platform",
+    ]
+    metadata = {
+      disable-legacy-endpoints = "true"
+    }
+    shielded_instance_config {
+      enable_secure_boot = true
+    }
+  }
+
+  management {
+    auto_upgrade = true
+    auto_repair  = true
+  }
+
+  autoscaling {
+    min_node_count = 1
+    max_node_count = 3
   }
 
   timeouts {
