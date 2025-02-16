8f9ed079241700cdcae13f531743831ae51f7f2d
david-gimelle <david.gimelle@gmail.com>
2025-02-16 17:09:19 +0000

    simplify cluster declaration

--- terraform/envs/dev/react/main.tf
+++ terraform/envs/dev/react/main.tf
@@ -15,7 +15,6 @@ terraform {
 
 provider "azurerm" {
   features {}
-
   skip_provider_registration = true
 
   client_id       = var.client_id
@@ -24,26 +23,35 @@ provider "azurerm" {
   subscription_id = var.subscription_id
 }
 
-module "react_cluster" {
-  source = "../../../modules/aks-cluster"
+resource "azurerm_kubernetes_cluster" "main" {
+  name                = var.cluster_name
+  location            = var.location
+  resource_group_name = var.resource_group_name
+  dns_prefix         = var.cluster_name
+  kubernetes_version = "1.26"
 
-  cluster_name           = var.cluster_name
-  location              = var.location
-  resource_group_name   = var.resource_group_name
-  kubernetes_version    = "1.26"
-  
-  # Node pool configuration
-  node_count            = 1
-  vm_size              = "Standard_B2s"
-  os_disk_size_gb      = 30
-  enable_auto_scaling  = true
-  min_count           = 1
-  max_count           = 3
+  default_node_pool {
+    name                = "default"
+    node_count          = 1
+    vm_size            = "Standard_B2s"
+    os_disk_size_gb    = 30
+    enable_auto_scaling = false
+  }
 
-  # Timeouts
-  create_timeout      = "30m"
-  update_timeout      = "30m"
-  delete_timeout      = "30m"
+  identity {
+    type = "SystemAssigned"
+  }
+
+  network_profile {
+    network_plugin    = "kubenet"
+    load_balancer_sku = "standard"
+  }
+
+  timeouts {
+    create = "30m"
+    update = "30m"
+    delete = "30m"
+  }
 
   tags = {
     Environment = "test"
