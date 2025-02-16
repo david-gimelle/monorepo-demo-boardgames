e309bba3c75903e4c15f1dd3a819367c2bd2382b
david-gimelle <david.gimelle@gmail.com>
2025-02-16 06:58:00 +0000

    cleanup kluster file

--- terraform/modules/aks-cluster/main.tf
+++ terraform/modules/aks-cluster/main.tf
@@ -24,11 +24,7 @@ resource "azurerm_kubernetes_cluster" "main" {
     load_balancer_sku = "standard"
   }
 
-  addon_profile {
-    http_application_routing {
-      enabled = true
-    }
-  }
+    http_application_routing_enabled = true
 
   auto_scaler_profile {
     scale_down_delay_after_add = "10m"
